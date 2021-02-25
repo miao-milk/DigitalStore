e56ce5c8ab6cd6d21744475138df0141580f51fa
mxw <1107346047@qq.com>
2021-02-25 10:58:08 +0800

    去掉多余文件

--- dStore-message/dStore-message-service/src/main/java/message/mapper/SendMessageMapper.java
+++
@@ -1,9 +0,0 @@
-package message.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.mxw.common.model.entity.MessageDO;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface SendMessageMapper extends BaseMapper<MessageDO> {
-}
